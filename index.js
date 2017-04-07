ab066f69bfe133aadde987c7f0e6fbd914341450
Daniel Mikusa <dmikusa@pivotal.io>
2017-04-06 23:23:55 -0400

    Import bootstrap and jquery to fix broken hamburger menu.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// import jquery and bootstrap
+var jQuery = require('jquery');
+window.$ = window.jQuery = jQuery;
+require('bootstrap');
+jQuery.noConflict(true);
+
+// import everything else
 import riot from 'riot'
 import AudioPlayer from './tags/audio-player.tag'
 import NoiseButton from './tags/noise-button.tag'
